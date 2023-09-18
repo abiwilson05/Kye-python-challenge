ee705d49ca7876a149832b93df85130d07e5e6d6
abiwilson05 <145335510+abiwilson05@users.noreply.github.com>
2023-09-18 10:27:16 +0100

    Create python challnges.py

---
+++ python challnges.py
@@ -0,0 +1,6 @@
+#area of a triangle
+base=int(input("please enter the base of the triangle"))
+height=int(input("please enter the height of the triangle (in the same units as the base)"))
+
+Trianglesarea=base*height/2
+print("the area of the triangle is: ", Trianglesarea)
